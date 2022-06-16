c5b0fda5669fd12d5ddbef49498c8ffc7ac2cda0
Vassa <vassatret@gmail.com>
2022-06-16 13:35:48 +0300

    Create main structure
    *feat: add blocks hero, about us, how we help, we`ve helped,news, contacts, footer
    *style: create variables
    *feat: add nav-scroll

---
+++ js/swiperslider.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 30,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    // when window width is >= 860px
+    860: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
