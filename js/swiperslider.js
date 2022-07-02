4a579d68c3046bcc456681f2fa3cf4fa10205819
Vassa <vassatret@gmail.com>
2022-07-02 18:59:26 +0300

    Reorganization and styling
    *docs: add Reasons and Decision
    *refactor: change buttons, style block Reasons and Decision
    *style: change icons for blocks, change colors
    *fix: change slider for news
    *docs: hide FAQ

--- js/swiperslider.js
+++ js/swiperslider.js
@@ -1,31 +1,31 @@
-const swiper = new Swiper('.swiper', {
+const helpSwiper = new Swiper('.help-swiper', {
   // Optional parameters
   loop: true,
 
   // Default parameters
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 10,
   // Responsive breakpoints
   breakpoints: {
     // when window width is >= 320px
     480: {
       slidesPerView: 1,
-      spaceBetween: 30
+      spaceBetween: 10
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 2,
-      spaceBetween: 40
+      spaceBetween: 10
     },
     // when window width is >= 860px
     860: {
       slidesPerView: 3,
-      spaceBetween: 40
+      spaceBetween: 10
     },
     // when window width is >= 1200px
     1200: {
       slidesPerView: 4,
-      spaceBetween: 40
+      spaceBetween: 20
     },
   },
 
@@ -39,4 +39,21 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.help-button-next',
     prevEl: '.help-button-prev',
   },
+});
+
+const newsSwiper = new Swiper('.news-swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.news-button-next',
+    prevEl: '.news-button-prev',
+  },
+
 });
