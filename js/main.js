ec4c7375ea5cfa5d51cee73214676a34dc58d0ec
Vassa <vassatret@gmail.com>
2022-08-02 20:01:03 +0300

    Page news structure + style
    *feat: blocks for news
    *feat: hero block
    *feat: link stories at index
    *fix: change names for imgs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+// 
 function i() {
   let custom = document.getElementById("custom3");
   let selectCustom = custom.options[custom.selectedIndex].value;
   let isCustom = selectCustom == "other"
   let show = document.getElementById("show");
   show.style.display = isCustom ? 'inherit' : 'none';
+};
+
+
+const achievementsBtn = document.querySelectorAll('.achievements-btn');
+for (let i = 0; i < achievementsBtn.length; i++) {
+  achievementsBtn[i].addEventListener('click', function () {
+    this.innerHTML =
+      (this.innerHTML === 'Read more') ? this.innerHTML = 'Roll up' : this.innerHTML = 'Read more';
+  })
 }
