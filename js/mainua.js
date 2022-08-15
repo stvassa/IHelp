709f22cf251c11242f487b96d14f0e00e0dea902
Vassa <vassatret@gmail.com>
2022-08-15 21:30:57 +0300

    docs: translate into ukr news page

---
+++ js/mainua.js
@@ -0,0 +1,17 @@
+// 
+function i() {
+  let custom = document.getElementById("custom3");
+  let selectCustom = custom.options[custom.selectedIndex].value;
+  let isCustom = selectCustom == "other"
+  let show = document.getElementById("show");
+  show.style.display = isCustom ? 'inherit' : 'none';
+};
+
+
+const achievementsBtn = document.querySelectorAll('.achievements-btn');
+for (let i = 0; i < achievementsBtn.length; i++) {
+  achievementsBtn[i].addEventListener('click', function () {
+    this.innerHTML =
+      (this.innerHTML === 'Читати більше') ? this.innerHTML = 'Згорнути' : this.innerHTML = 'Читати більше';
+  })
+}
