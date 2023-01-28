06d110742e6eb2dccb34bc4a47eb69cb1338cd64
Vassa <vassatret@gmail.com>
2023-01-28 18:32:17 +0200

    refactor:change  questionnaire and course files

---
+++ uk/questionnaire.php
@@ -0,0 +1,58 @@
+<?php
+// Импортируем классы PHPMailer в глобальное пространство имен
+// Эти строки должны быть вначале скрипта, не внутри функции
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Формирование самого письма
+$title = "Course form";
+$body = "Тест";
+
+// Настройки PHPMailer
+$mail = new PHPMailer(true);
+
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;   //Enable verbose debug output
+    $mail->isSMTP();                         //Send using SMTP
+    $mail->CharSet = "UTF-8";
+    $mail->Host       = 'smtp.gmail.com';    //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                //Enable SMTP authentication
+    $mail->Username   = 'ihelpuw@gmail.com'; //SMTP username
+    $mail->Password   = 'wwapbtqnlnzhzhqp';  //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;//Enable implicit TLS encryption
+    $mail->Port       = 465;    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('ihelpuw@gmail.com', 'Елена Рязанова');
+    $mail->addAddress($email);    //Add a recipient 
+       
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "The message was not sent. Cause of error: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: success.html');
+?>
